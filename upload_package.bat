73d3fcf93ae1dbac5be97d4a8cf7a9f504cf52f4
Codanium <cricketdrop6@gmail.com>
2023-12-15 18:32:38 +0530

    🔧 chore: Update upload_package.bat with new API token
    - Update the `upload_package.bat` file to replace the previous API token with a new one.
    - The new token is more secure and will be used to authenticate the package upload to PyPI.

--- upload_package.bat
+++ upload_package.bat
@@ -9,4 +9,4 @@ REM Build the package
 python setup.py sdist bdist_wheel
 
 REM Upload the package using twine
-twine upload --repository pypi dist/* -u __token__ -p pypi-AgEIcHlwaS5vcmcCJDhiYTk5MGRlLTlmMzQtNGU3ZS04ZTQ2LWQ4NjUwMmZlNWIzMwACKlszLCIzNTNkNDRiMS0zMWJlLTQ5YzItOTQ1Mi1jOWIwMjNkNjFmMWYiXQAABiB8XPCV88CJ7icUxSoLC_c-ywow18nzyAPCmmOOxezzpQ
+twine upload --repository pypi dist/* -u __token__ -p api-token
