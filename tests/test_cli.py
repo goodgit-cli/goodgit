94d677c05704d1f4a3719c1f4a64ab88ad7cada1
zaibutcooler <zaiyellyintaung@gmail.com>
2024-04-20 11:15:21 +0630

    added demo test case

---
+++ tests/test_cli.py
@@ -0,0 +1,11 @@
+import unittest
+from unittest.mock import patch
+from io import StringIO
+from goodgit import GoodGit
+
+class TestGoodGitCLI(unittest.TestCase):
+    def test_demo(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
